e2ff8ef77366618d9eb1f084b45a7a403387b7dc
steveMQ <steveMQ@users.noreply.github.com>
2022-08-12 18:37:51 -0400

    optimizations

--- calc.js
+++ calc.js
@@ -2,54 +2,57 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+const romanToInt = (s) => {
     
-    s = "MMMLVII"
-    
-    const edgeCases = [
-        "IV",
-        "IX",
-        "XL",
-        "XC",
-        "CD",
-        "CM"
-    ]
-    
-    const splitEdgeCases = (str, edges) => {
-      const edgesArr = edges.map(item => {
-        if(s.includes(item) === true){
-          str = str.replace(item, '')
-          return item;
-        }
-        return null;
-      })
-      .filter(item => item !== null);
-      return {normal: str.split(''), edge: edgesArr}
-    }
-    const assignValues = (data, modifier) => {
-        
-        const values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        } 
-        
-        return data.map(item => {
-            
-            if(modifier){
-                return (values[item.split('')[0]] * -1) + (values[item.split('')[1]]);
-            }
-            return values[item];
-        })
-    }
-    
-    const dataSets = splitEdgeCases(s, edgeCases);
 
-    return assignValues(dataSets.edge, true)
-            .concat(assignValues(dataSets.normal))
-            .reduce((a, b) => a + b, 0);    
+    
+  const splitEdgeCases = (str) => {
+
+    const edgeCases = [
+      "IV",
+      "IX",
+      "XL",
+      "XC",
+      "CD",
+      "CM"
+    ];
+
+    const edgesArr = edgeCases.map(item => {
+      if(str.includes(item)){
+        str = str.replace(item, '')
+        return item;
+      }
+      return null;
+    })
+    .filter(item => item !== null);
+    return {normal: str.split(''), edge: edgesArr}
+  }
+  
+  const assignValues = (data, modifier) => {
+      
+      const values = {
+          "I": 1,
+          "V": 5,
+          "X": 10,
+          "L": 50,
+          "C": 100,
+          "D": 500,
+          "M": 1000
+      } 
+      
+      return data.map(item => {
+          if(modifier){
+              return (values[item.split('')[0]] * -1) + (values[item.split('')[1]]);
+          }
+          return values[item];
+      })
+  }
+    
+  const dataSets = splitEdgeCases(s);
+
+  return assignValues(dataSets.edge, true)
+          .concat(assignValues(dataSets.normal))
+          .reduce((a, b) => a + b, 0);    
 };
+
+console.log(romanToInt("MMMLVII"));
